c217de7176531eeeb86078301bfe7da334ffa640
com/ngoranjeanmichelvianney-droid <ngoranjeanmichelvianney@gmail.com>
2026-07-29 11:35:18 +0000

    cdfggg

--- src/middleware.js
+++ src/middleware.js
@@ -68,5 +68,7 @@ export async function middleware(request) {
 }
 
 export const config = {
-  matcher: ["/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)"],
+  matcher: [
+    "/((?!_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+  ],
 };
